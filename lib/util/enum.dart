2fb4ba8de599b2559ffb3e5afba6faf9cbd4024d
Jamorn Anantariyakul <jamorn@pluseducenter.com>
2025-02-07 11:07:52 +0700

    Update - add enum

--- lib/util/enum.dart
+++ lib/util/enum.dart
@@ -5,15 +5,71 @@ enum WorkFlowStatus {
   treatment,
   monitoring,
   assistance,
-  discharged
+  discharged;
+
+  static WorkFlowStatus? setValue(String? value) {
+    switch (value) {
+      case "REGISTERING":
+        return WorkFlowStatus.registering;
+      case "SCREENING":
+        return WorkFlowStatus.screening;
+      case "TREATMENT":
+        return WorkFlowStatus.treatment;
+      case "MONITORING":
+        return WorkFlowStatus.monitoring;
+      case "ASSISTANCE":
+        return WorkFlowStatus.assistance;
+      case "DISCHARGED":
+        return WorkFlowStatus.discharged;
+      default:
+        return null;
+    }
+  }
 }
 // ตัวกรอง ผู้ใช้ ผู้เสพ ผู้ติด
 
 // OAS เขียว เหลือง ส้ม แดง
-enum LevelType { normal, semiUrgency, urgency, emergency }
+enum LevelType {
+  normal,
+  semiUrgency,
+  urgency,
+  emergency;
+
+  static LevelType? setValue(String? value) {
+    switch (value) {
+      case "NORMAL":
+        return LevelType.normal;
+      case "SEMI_URGENCY":
+        return LevelType.semiUrgency;
+      case "URGENCY":
+        return LevelType.urgency;
+      case "EMERGENCY":
+        return LevelType.emergency;
+      default:
+        return null;
+    }
+  }
+}
 
 // V2
-enum DrugEvalResult { user, abuse, dependence }
+enum DrugEvalResult {
+  user,
+  abuse,
+  dependence;
+
+  static DrugEvalResult? setValue(String? value) {
+    switch (value) {
+      case "USER":
+        return DrugEvalResult.user;
+      case "ABUSE":
+        return DrugEvalResult.abuse;
+      case "DEPENDENCE":
+        return DrugEvalResult.dependence;
+      default:
+        return null;
+    }
+  }
+}
 
 // ประเภท การบำบัดรักษาล่าสุด
 enum TreatmentType {
@@ -29,11 +85,55 @@ enum TreatmentType {
   wiwatSchoolProject,
   programInPrisons,
   behaviorCamp,
-  other
+  other;
+
+  static TreatmentType? setValue(String? value) {
+    switch (value) {
+      case "OPD":
+        return TreatmentType.opd;
+      case "IPD_TREATMENT":
+        return TreatmentType.ipdTreatment;
+      case "IPD_RECOVER":
+        return TreatmentType.ipdRecover;
+      case "IPD_MINI":
+        return TreatmentType.ipdMini;
+      case "CBTX":
+        return TreatmentType.cbtx;
+      case "NETWORK":
+        return TreatmentType.network;
+      case "RELIGIOUS":
+        return TreatmentType.religious;
+      case "REHABILITATION_THERAPY_JOPC":
+        return TreatmentType.rehabilitationTherapyJopc;
+      case "REHABILITATION_THERAPY_TRAINING_CENTER":
+        return TreatmentType.rehabilitationTherapyTrainingCenter;
+      case "WIWAT_SCHOOL_PROJECT":
+        return TreatmentType.wiwatSchoolProject;
+      case "PROGRAM_IN_PRISONS":
+        return TreatmentType.programInPrisons;
+      case "BEHAVIOR_CAMP":
+        return TreatmentType.behaviorCamp;
+      case "OTHER":
+        return TreatmentType.other;
+      default:
+        return null;
+    }
+  }
 }
 
 //สถานะเพิ่มเติม
-enum SmivType { smiv }
+enum SmivType {
+  smiv;
+
+  static SmivType? setValue(String? value) {
+    switch (value) {
+      case "SMIV":
+        return SmivType.smiv;
+      default:
+        return null;
+    }
+  }
+}
 
 enum AppointmentType {
   assistance,
@@ -57,18 +157,20 @@ enum AppointmentType {
   }
 }
 
-extension AppointmentTypeExtension on AppointmentType {
-  String? get value {
-    switch (this) {
-      case AppointmentType.assistance:
-        return "ASSISTANCE";
-      case AppointmentType.monitoring:
-        return "MONITORING";
-      case AppointmentType.treatment:
-        return "TREATMENT";
-      case AppointmentType.reject:
-        return "REJECT";
-      case null:
+enum CertificateStatus {
+  request,
+  completed,
+  denied;
+
+  static CertificateStatus? setValue(String? value) {
+    switch (value) {
+      case "REQUEST":
+        return CertificateStatus.request;
+      case "COMPLETED":
+        return CertificateStatus.completed;
+      case "DENIED":
+        return CertificateStatus.denied;
+      default:
         return null;
     }
   }
