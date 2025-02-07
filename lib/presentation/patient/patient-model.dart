f1d2773b6c9b122fcaf7711b3194382589b7d6ff
Jamorn Anantariyakul <jamorn@pluseducenter.com>
2025-02-07 10:36:49 +0700

    Update - patient model

--- lib/presentation/patient/patient-model.dart
+++ lib/presentation/patient/patient-model.dart
@@ -1,25 +1,25 @@
-import 'package:bst_staff_mobile/data/repository/patient-repository.dart';
-import 'package:bst_staff_mobile/domain/model/patient.dart';
-import 'package:bst_staff_mobile/domain/service/app_service.dart';
-import 'package:logger/logger.dart';
+// import 'package:bst_staff_mobile/data/repository/patient-repository.dart';
+// import 'package:bst_staff_mobile/domain/model/patient.dart';
+// import 'package:bst_staff_mobile/domain/service/app_service.dart';
+// import 'package:logger/logger.dart';
 
-class PatientModel {
-  final Logger log;
-  final PatientRepository patientRepository;
-  final AppService appService;
+// class PatientModel {
+//   final Logger log;
+//   final PatientRepository patientRepository;
+//   final AppService appService;
 
-  PatientModel({
-    required this.log,
-    required this.patientRepository,
-    required this.appService,
-  });
+//   PatientModel({
+//     required this.log,
+//     required this.patientRepository,
+//     required this.appService,
+//   });
 
-  late PatientAll patients;
-  int page = 0;
-  String name = "";
+//   late PatientAll patients;
+//   int page = 0;
+//   String name = "";
 
-  Future<PatientAll> loadData() async {
-    return patients =
-        await patientRepository.findPatientAll(searchVal: name, page: page);
-  }
-}
+//   Future<PatientAll> loadData() async {
+//     return patients =
+//         await patientRepository.findPatientAll(searchVal: name, page: page);
+//   }
+// }
